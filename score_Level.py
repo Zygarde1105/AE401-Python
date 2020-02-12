4114340d670dfed4c393d68c0411da3ef1d06307
Zygarde1105 <60865309+Zygarde1105@users.noreply.github.com>
2020-02-12 12:06:03 +0800

    score_level

---
+++ score_Level.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 11 19:58:56 2020
+
+@author: Richard_Surface1
+"""
+
+score = int(input("What is your score"))
+if score >= 90:
+    print("A")
+elif score >= 80:
+    print("B")
+elif score >= 70:
+    print("C")
+elif score >= 60:
+    print("D")
+else:
+    print("F")
+
+    
+    
