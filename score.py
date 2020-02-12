c2df79c4ab1a29478d24bb2a3511f691f77d7681
Zygarde1105 <60865309+Zygarde1105@users.noreply.github.com>
2020-02-12 12:06:24 +0800

    score

---
+++ score.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 11 19:37:39 2020
+
+@author: Richard_Surface1
+"""
+
+score = int(input("What is your score?"))
+
+if score < 60:
+    print("so bad")
+else:
+    print("so good")
